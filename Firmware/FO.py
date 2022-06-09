60caac97dda83cc0a5f9fa3f80ca35da69df5d17
Daniele Bella <danielebella2000@gmail.com>
2022-06-09 17:04:41 +0100

    Add initial skeleton

---
+++ Firmware/FO.py
@@ -0,0 +1,10 @@
+
+class FlyingObject:
+    '''
+    The object that Pickle Rick has been inserted into, and is being tracked
+    '''
+
+    def __init__():
+        self._px = 0
+        self._py = 0
+        self._pz = 0
---
+++ Firmware/turret.py
@@ -0,0 +1,15 @@
+
+class servoPointer:
+    '''
+    The system of 2 servos that are used to point at the flying object
+    '''
+
+class turretGPS:
+    '''
+    USB GPS connected to Dom Turretto
+    '''
+
+class Turret:
+    '''
+    Dom Turretto. Has a servo system, and a GPS receiver.
+    '''
